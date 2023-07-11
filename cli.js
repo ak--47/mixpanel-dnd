40539a6a9ce0f2b086534b1a1e84d78448bca213
AK <akrivit@gmail.com>
2023-07-11 16:47:06 -0400

    get server working

--- cli.js
+++ cli.js
@@ -1,4 +1,5 @@
 #! /usr/bin/env node
+process.env.NODE_ENV='local' //todo: don't do this here...
 import esMain from 'es-main';
 import server from './server/server.js';
 
@@ -7,7 +8,7 @@ async function main() {
 	console.log('disclaimer: this product is alpha\n\n')
 	console.log('app is alive! visit...\n')
 	console.log('\thttp://localhost:3000/\n')
-	console.log('\n...when finished press CTRL+C to kill the local server')
+	console.log('\n...when finished press CTRL+C to kill the local server\n')
 }
 
 export default main;
